573fe845cd7111b9fa30f896f6687ba91e6ee6d4
Saurav Kumar <csaurav2014@gmail.com>
2025-11-14 17:51:22 +0530

    Getting Started with Product Component to Set Layout

--- quickmart_ecommerce/src/App.jsx
+++ quickmart_ecommerce/src/App.jsx
@@ -1,14 +1,15 @@
 
 import { FaBeer } from 'react-icons/fa'
 import './App.css'
+import Products from './components/Products'
 
 function App() {
   
 
   return (
-    <h1 class="flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold">
-    Hello world! <FaBeer />
-  </h1>
+    <>
+      <Products />
+    </>
   )
 }
 
