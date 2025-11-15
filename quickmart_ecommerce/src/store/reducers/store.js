dd1b4177f8f941f5ecdd727f7fc21d585a8effdf
Saurav Kumar <csaurav2014@gmail.com>
2025-11-15 14:04:07 +0530

    Fetching Product from API

--- quickmart_ecommerce/src/store/reducers/store.js
+++ quickmart_ecommerce/src/store/reducers/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { productReducer } from "./ProductReducer";
 
 export const store = configureStore({
-    reducer: {},
-    preloadedState:{},
+    reducer: {
+        products: productReducer,
+    },
+    preloadedState:{     
+    },
 });
 
 export default store;
