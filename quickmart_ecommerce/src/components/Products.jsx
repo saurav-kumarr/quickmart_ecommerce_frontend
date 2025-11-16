e49ec541fe2df41f652db8fbd7d4758fa3b6de95
Saurav Kumar <csaurav2014@gmail.com>
2025-11-16 18:00:41 +0530

    Building Pagination For Our Products

--- quickmart_ecommerce/src/components/Products.jsx
+++ quickmart_ecommerce/src/components/Products.jsx
@@ -5,54 +5,51 @@ import { fetchCategories } from "../store/actions";
 import { useEffect } from "react";
 import Filter from "./Filter";
 import useProductFilter from "./useProductFilter";
+import Loader from "./Loader";
+import Paginations from "./Paginations";
 
 // 1. Make sure url is updated with filter values
 // 2. Use this filter values for getting data from backend
 
 const Products = () => {
+  const { isLoading, errorMessage } = useSelector((state) => state.errors);
 
-    const { isLoading, errorMessage } = useSelector(
-        (state) => state.errors
-    );
+  const { products, categories, pagination } = useSelector((state) => state.products);
+  const dispatch = useDispatch();
 
-    const {products, categories} = useSelector(
-        (state) => state.products
-    )
-    const dispatch = useDispatch();
+  useProductFilter();
 
-    useProductFilter();
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(fetchCategories());
-    },[dispatch]);
-
-
-
-
-    return (
-        <div className="lg:px-14 sm:px-8 px-4 py-14 2xl:wl-[90%] 2xl:mx-auto">
-            <Filter categories={categories ? categories : []}/>
-            {isLoading ? (
-                <p>It is loading...</p>
-            ) : errorMessage ? (
-                <div className="flex justify-center items-center h-[200px]">
-                    <FaExclamationTriangle className="text-slate-800 text-3xl mr-2"/>
-                    <span className="text-slate-800 text-lg font-medium">
-                        {errorMessage}
-                    </span>
-                </div>
-            ) : (
-                <div className="min-h-[700px]">
-                    <div className="pb-6 pt-14 grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-y-6 gap-x-6">
-                        {products &&
-                            products.map((item, i) => <ProductCard key={i} {...item} />
-
-                            )}
-                    </div>
-                </div>
-            )}
+  return (
+    <div className="lg:px-14 sm:px-8 px-4 py-14 2xl:wl-[90%] 2xl:mx-auto">
+      <Filter categories={categories ? categories : []} />
+      {isLoading ? (
+        <Loader text={""} />
+      ) : errorMessage ? (
+        <div className="flex justify-center items-center h-[200px]">
+          <FaExclamationTriangle className="text-slate-800 text-3xl mr-2" />
+          <span className="text-slate-800 text-lg font-medium">
+            {errorMessage}
+          </span>
         </div>
-    )
-}
+      ) : (
+        <div className="min-h-[700px]">
+          <div className="pb-6 pt-14 grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-y-6 gap-x-6">
+            {products &&
+              products.map((item, i) => <ProductCard key={i} {...item} />)}
+          </div>
+          <div className="flex justify-center pt-10">
+            <Paginations 
+            numberOfPage = {pagination?.totalPages}
+            totalProducts = {pagination?.totalElements}/>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Products;
