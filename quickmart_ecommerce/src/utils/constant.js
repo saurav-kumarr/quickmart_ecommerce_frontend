a8a71769045f13487881b9d7b570f06db03663ff
Saurav Kumar <csaurav2014@gmail.com>
2025-11-17 00:51:06 +0530

    Setting Up Banner on Home Page

---
+++ quickmart_ecommerce/src/utils/constant.js
@@ -0,0 +1,9 @@
+import bannerImageOne from "../assets/sliders/one.png"
+import bannerImageTwo from "../assets/sliders/two.png"
+import bannerImageThree from "../assets/sliders/three.png"
+
+export {
+    bannerImageOne,
+    bannerImageTwo,
+    bannerImageThree
+}
