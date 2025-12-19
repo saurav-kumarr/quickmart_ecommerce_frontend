c5a5f37e383766314ef686c869dcd3eb35ebdfe4
Saurav Kumar <csaurav2014@gmail.com>
2025-12-20 00:46:36 +0530

    Setting up Login Interface

--- quickmart_ecommerce/src/components/auth/Login.jsx
+++ quickmart_ecommerce/src/components/auth/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AiOutlineLogin } from "react-icons/ai";
 import InputField from "../shared/InputField";
 
@@ -34,7 +34,7 @@ const LogIn = () => {
                 <div className="flex flex-col gap-3">
                     <InputField 
                         label="UserName"
-                        reuired
+                        required
                         id="username"
                         type="text"
                         message="*UserName is required"
@@ -44,7 +44,7 @@ const LogIn = () => {
 
                     <InputField 
                         label="Password"
-                        reuired
+                        required
                         id="password"
                         type="password"
                         message="*Password is required"
@@ -63,6 +63,17 @@ const LogIn = () => {
                         <>Login</>
                     )}
                 </button>
+                <p
+                  className="text-center text-sm text-slate-700 mt-6">
+                    Don't have an account?
+                    <Link 
+                     className="font-semibold underline hover:text-black" 
+                      to="/register">
+                    <span>
+                        SignUp
+                    </span>
+                    </Link>
+                </p>
             </form>
         </div>
       )
