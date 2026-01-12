5ed9759d56bc2409fb9dfdd2f8014748cbc21dbc
Saurav Kumar <csaurav2014@gmail.com>
2026-01-13 00:59:13 +0530

    Setting Up the Admin Product Management Page Layout

--- quickmart_ecommerce/src/components/admin/products/AdminProducts.jsx
+++ quickmart_ecommerce/src/components/admin/products/AdminProducts.jsx
@@ -1,8 +1,113 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {MdAddShoppingCart} from 'react-icons/md';
+import { useSelector } from 'react-redux';
+import Loader from '../../shared/Loader';
+import { FaBoxOpen } from 'react-icons/fa';
+import { DataGrid } from '@mui/x-data-grid';
+import { adminProductTableColumn } from '../../helper/tableColumn';
 const AdminProducts = () => {
+
+  const products = [{ "productName": "Robot3", "image": "http://localhost:8080/images/4cc3c95e-7d34-4e43-8e4d-c933bcc2b361.png", "description": "Its an automatic car based on voice recognition & equiped with machine gun.", "quantity": 15, "price": 400.0, "discount": 0.0, "specialPrice": 400.0, "productId": 4 }, { "productName": "Robot2", "image": "http://localhost:8080/images/1aecfd5a-2eb8-42fc-bf19-2ef298c46ec7.png", "description": "Its an automatic car based on voice recognition & equiped with machine gun.", "quantity": 15, "price": 400.0, "discount": 0.0, "specialPrice": 400.0, "productId": 3 }];
+  const pagination = {pageNumber: 0, pageSize: 10, totalElements: 2, totalPages: 1, lastPage: true};
+  const {isLoading, errorMessage} = useSelector((state) => state.errors);
+  
+  const tableRecords = products?.map((item) => {
+  return {
+    id: item.productId,
+    productName: item.productName,
+    description: item.description,
+    discount: item.discount,
+    image: item.image,
+    price:item.price,
+    quantity:item.quantity,
+    specialPrice: item.specialPrice,
+  }
+});
+
+const [currentPage, setCurrentPage] = useState(
+    pagination?.pageNumber + 1 || 1
+  );
+
+  const handleEdit = (product) => {
+
+  };
+  const handleDelete = (product) => {
+
+  };
+  const handleImageUpload = (product) => {
+
+  };
+  const handleProductView = (product) => {
+
+  };
+  const handlePaginationChange = (paginationModel) => {
+
+  };
+
+  const emptyProduct =  !products || products?.length === 0;
+  
   return (
-    <div>AdminProducts</div>
+    <div>
+      <div className='pt-6 pb-10 flex justify-end'>
+        <button
+           className='bg-custom-blue hover:bg-blue-800 text-white font-semibold py-2 px-4 flex items-center gap-2 rounded-md shadow-md transition-colors hover:text-slate-300 duration-300'>
+          <MdAddShoppingCart className='text-xl'/>
+          Add Product
+        </button>
+      </div>
+
+      {!emptyProduct && (
+        <h1
+         className='text-slate-800 text-3xl text-center font-bold pb-6 uppercase'>
+        All Products</h1>
+      )}
+      {isLoading ? (
+        <Loader /> 
+      ) : (
+        <>
+        {emptyProduct ? (
+          <div className='flex flex-col items-center justify-center text-gray-600 py-10'>
+           <FaBoxOpen size={50} className='mb-3'/>
+            <h2 className='text-2xl font-semibold'>
+              No product created yet
+            </h2>
+          </div>
+        ) : (
+          <div className='max-w-full'>
+            <DataGrid
+                    className='w-full '
+                    rows={tableRecords}
+                    columns={adminProductTableColumn(
+                      handleEdit,
+                      handleDelete,
+                      handleImageUpload,
+                      handleProductView)}
+                    paginationMode='server'
+                    rowCount={pagination?.totalElements || 0}
+                    initialState={{
+                      pagination: {
+                        paginationModel: {
+                          pageSize: pagination?.pageSize || 10,
+                          page: currentPage - 1,
+                        },
+                      },
+                    }}
+                    onPaginationModelChange={handlePaginationChange}
+                    disableRowSelectionOnClick
+                    pageSizeOptions={[pagination?.pageSize || 10]}
+                    pagination
+                    paginationOptions={{
+                      showFirstButton: true,
+                      showLastButton: true,
+                      hideNextButton: currentPage === pagination?.totalPages,
+                    }}
+                  />
+          </div>
+        )}
+        </>
+      )}
+
+    </div>
   )
 }
 
--- quickmart_ecommerce/src/components/helper/tableColumn.jsx
+++ quickmart_ecommerce/src/components/helper/tableColumn.jsx
@@ -1,6 +1,155 @@
 
-import { FaEdit } from 'react-icons/fa';
+import { FaEdit, FaEye, FaImage, FaTrashAlt } from 'react-icons/fa';
 
+
+export const adminProductTableColumn =(
+  handleEdit,
+  handleDelete,
+  handleImageUpload,
+  handleProductView
+) => [
+  { 
+    // ORDER ID
+    sortable: false,
+    disableColumnMenu: true,
+    field: "id",
+    headerName: "ID",
+    minWidth: 180,
+    headerAlign: "center",
+    editable: false,
+    headerClassName: "text-black font-semibold border",
+    cellClassName: "text-slate-700 font-normal border",
+    renderHeader: (params) => <span className='text-center'>Product ID</span>
+   },
+  {
+    //Column for customer email
+    sortable: false,
+    disableColumnMenu: true,
+    field: "productName",
+    headerName: "productName",
+    align: "center",
+    Width: 260,
+    headerAlign: "center",
+    editable: false,
+    headerClassName: "text-black font-semibold text-center border",
+    cellClassName: "text-slate-700 font-normal border text-center",
+    renderHeader: (params) => <span>Product Name</span>
+  },
+  {
+    // column for showing total amount of the Order.
+    sortable: true,
+    disableColumnMenu: true,
+    field: "price",
+    headerName: "Price",
+    align: "center",
+    Width: 200,
+    headerAlign: "center",
+    editable: false,
+    headerClassName: "text-black font-semibold text-center border",
+    cellClassName: "text-slate-700 font-normal border text-center",
+    renderHeader: (params) => <span>Price</span>
+  },
+  {
+    // Column to display order status (e.g., Pending, Shipped).
+    sortable: false,
+    disableColumnMenu: true,
+    field: "quantity",
+    headerName: "Quantity",
+    align: "center",
+    Width: 200,
+    headerAlign: "center",
+    editable: false,
+    headerClassName: "text-black font-semibold text-center border",
+    cellClassName: "text-slate-700 font-normal border text-center",
+    renderHeader: (params) => <span>Quantity</span>
+  },
+  {
+    // Column for order creation date.
+    sortable: false,
+    disableColumnMenu: true,
+    field: "specialPrice",
+    headerName: "Price",
+    align: "center",
+    Width: 200,
+    headerAlign: "center",
+    editable: false,
+    headerClassName: "text-black font-semibold text-center border",
+    cellClassName: "text-slate-700 font-normal border text-center",
+    renderHeader: (params) => <span>Special Price</span>
+  },
+  {
+    // Column for order creation date.
+    sortable: false,
+    disableColumnMenu: true,
+    field: "description",
+    headerName: "Description",
+    align: "center",
+    Width: 200,
+    headerAlign: "center",
+    editable: false,
+    headerClassName: "text-black font-semibold text-center border",
+    cellClassName: "text-slate-700 font-normal border text-center",
+    renderHeader: (params) => <span>Description</span>
+  },
+  {
+    // Column for order creation date.
+    sortable: false,
+    disableColumnMenu: true,
+    field: "image",
+    headerName: "Image",
+    align: "center",
+    Width: 200,
+    headerAlign: "center",
+    editable: false,
+    headerClassName: "text-black font-semibold text-center border",
+    cellClassName: "text-slate-700 font-normal border text-center",
+    renderHeader: (params) => <span>Image</span>
+  },
+  {
+    // Custom action column with an "Edit" button.
+    sortable: false,
+    //disableColumnMenu: true,
+    field: "action",
+    headerName: "Action",
+    //align: "center",
+    Width: 400,
+    headerAlign: "center",
+    editable: false,
+    headerClassName: "text-black font-semibold text-center ",
+    cellClassName: "text-slate-700 font-normal",
+    renderHeader: (params) => <span>Action</span>,
+    renderCell: (params) => {
+      return(
+        <div className='flex justify-center items-center space-x-2 h-full pt-2'>
+          <button
+                onClick={() => handleImageUpload(params.row)}
+                className='flex items-center bg-green-500 hover:bg-green-700 text-white px-4 h-9 rounded-md'>
+                <FaImage className='mr-2'/>
+                Image
+          </button>
+          <button
+            onClick={() => handleEdit(params.row)}
+            className='flex items-center bg-blue-500 text-white px-4 h-9 rounded-md'>
+            <FaEdit className='mr-2' />
+            Edit
+          </button>
+          <button
+            onClick={() => handleDelete(params.row)}
+            className='flex items-center bg-red-500 text-white px-4 h-9 rounded-md'>
+            <FaTrashAlt className='mr-2' />
+            Delete
+          </button>
+          <button
+            onClick={() => handleProductView(params.row)}
+            className='flex items-center bg-slate-800 text-white px-4 h-9 rounded-md'>
+            <FaEye className='mr-2' />
+            View
+          </button>
+        </div>
+      );
+    },
+  },
+];
 
 export const adminOrderTableColumn =(handleEdit) => [
   { 
