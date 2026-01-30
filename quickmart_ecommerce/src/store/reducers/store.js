fe755260a2f78b5724c31b4753a25e1e81ad24e0
Saurav Kumar <csaurav2014@gmail.com>
2026-01-30 21:01:20 +0530

    Developing the Seller Management Page for Admin Dashboard

--- quickmart_ecommerce/src/store/reducers/store.js
+++ quickmart_ecommerce/src/store/reducers/store.js
@@ -6,6 +6,7 @@ import { authReducer } from "./authReducer";
 import { paymentMethodReducer } from "./paymentMethodReducer";
 import { adminReducer } from "./adminReducer";
 import { orderReducer } from "./orderReducer";
+import { sellerReducer } from "./sellerReducer";
 
 
 const user = localStorage.getItem("auth")
@@ -34,6 +35,7 @@ export const store = configureStore({
         payment: paymentMethodReducer,
         admin: adminReducer,
         order: orderReducer,
+        seller: sellerReducer,
     },
     preloadedState: initialState,
 });
